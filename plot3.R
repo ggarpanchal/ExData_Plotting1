61e4876c5feff64e7b8f3ad5c0f8931ecf1fbb71
Jigar Panchal <panchal.jigar90@gmail.com>
2019-03-12 15:46:02 +0530

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,25 @@
+file_name <- "household_power_consumption.zip"
+
+if (!file.exists(file_name)){
+  URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip "
+  download.file( URL, file_name, method= "curl" )
+}  
+if (!file.exists("household_power_consumption.txt")) { 
+  unzip(file_name) 
+}
+
+power_c_df <- data.table::fread("household_power_consumption.txt")
+trim_df <- (dplyr::filter(power_c_df, Date == "1/2/2007"| Date == "2/2/2007" ))
+trim_df$timedate <- lubridate::dmy(trim_df$Date) + lubridate::hms(trim_df$Time)
+
+png(filename = "plot3.png", bg = "transparent", width = 480, height = 480)
+
+with(trim_df, plot( timedate,Sub_metering_1 , type = "l", 
+                     ylab = "Energy sub metering", xlab = NA))
+with(trim_df, points(timedate,Sub_metering_2, col= "Red",type = "l"))
+with(trim_df, points(timedate,Sub_metering_3, col= "Blue",type = "l"))
+legend("topright",  col = c("black", "Red", "Blue"),
+       legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lwd = 2)
+
+
+dev.off()
