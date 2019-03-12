9168519db5bbed3c6975f4a1b1d5123cc3f35fea
Jigar Panchal <panchal.jigar90@gmail.com>
2019-03-12 15:29:19 +0530

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,20 @@
+file_name <- "household_power_consumption.zip"
+
+if (!file.exists(file_name)){
+  URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip "
+  download.file( URL, file_name, method= "curl" )
+}  
+if (!file.exists("household_power_consumption.txt")) { 
+  unzip(file_name) 
+}
+
+power_c_df <- data.table::fread("household_power_consumption.txt")
+trim_df <- (dplyr::filter(power_c_df, Date == "1/2/2007"| Date == "2/2/2007" ))
+trim_df$timedate <- lubridate::dmy(trim_df$Date) + lubridate::hms(trim_df$Time)
+
+png(filename = "plot2.png", bg = "transparent")
+
+plot(trim_df$timedate, trim_df$Global_active_power, type = "l", xlab = NA, 
+     ylab = "Global Active Power(kilowatts)")
+
+dev.off()
