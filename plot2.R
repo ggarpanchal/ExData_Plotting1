3fd689e6047e7c94ee4efb53274e065cc79abf36
Jigar Panchal <panchal.jigar90@gmail.com>
2019-03-12 18:56:54 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,17 +1,21 @@
 file_name <- "household_power_consumption.zip"
 
 if (!file.exists(file_name)){
-  URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip "
+  URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
   download.file( URL, file_name, method= "curl" )
 }  
 if (!file.exists("household_power_consumption.txt")) { 
   unzip(file_name) 
 }
 
+# Load whole data frame to variable "power_c_df"
 power_c_df <- data.table::fread("household_power_consumption.txt")
-trim_df <- (dplyr::filter(power_c_df, Date == "1/2/2007"| Date == "2/2/2007" ))
-trim_df$timedate <- lubridate::dmy(trim_df$Date) + lubridate::hms(trim_df$Time)
 
+# Fatch "1/2/2007" & "2/2/2007" day from data frame and store in "trim_df"
+trim_df <- (dplyr::filter(power_c_df, Date == "1/2/2007"| Date == "2/2/2007" ))
+trim_df$Global_active_power <- as.numeric(trim_df$Global_active_power)
+
+# "plot1.png" named png file graphic device ON
 png(filename = "plot2.png", bg = "transparent")
 
 plot(trim_df$timedate, trim_df$Global_active_power, type = "l", xlab = NA, 
