04c8ac5838c5b56e3347a6366d1f030064fdc027
Jigar Panchal <panchal.jigar90@gmail.com>
2019-03-13 10:01:18 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -14,6 +14,7 @@ power_c_df <- data.table::fread("household_power_consumption.txt")
 # Fatch "1/2/2007" & "2/2/2007" day from data frame and store in "trim_df"
 trim_df <- (dplyr::filter(power_c_df, Date == "1/2/2007"| Date == "2/2/2007" ))
 trim_df$Global_active_power <- as.numeric(trim_df$Global_active_power)
+trim_df$timedate <- dmy(trim_df$Date) + hms(trim_df$Time)
 
 # "plot1.png" named png file graphic device ON
 png(filename = "plot2.png", bg = "transparent")
